14f842691ba666c343e6cf66ba9dd0f2699218d4
Nikita Chupahin <nik123chupahin@gmail.com>
2024-07-05 12:59:18 +0300

    Implemented not-found.html and updated GlobalExceptionHandle

--- src/main/java/com/softserve/itacademy/exception/GlobalExceptionHandler.java
+++ src/main/java/com/softserve/itacademy/exception/GlobalExceptionHandler.java
@@ -1,5 +1,32 @@
 package com.softserve.itacademy.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.persistence.EntityNotFoundException;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ModelAndView handleEntityNotFoundException(EntityNotFoundException exception) {
+        return getModelAndView(HttpStatus.NOT_FOUND, exception, "not-found");
+    }
+
+    private ModelAndView getModelAndView(HttpStatus httpStatus, Exception exception, String viewName) {
+        logger.error("An exception '{}' was raised", exception.getMessage());
+        ModelAndView modelAndView = new ModelAndView(viewName);
+        modelAndView.addObject("code", httpStatus.value());
+        modelAndView.addObject("phrase", httpStatus.getReasonPhrase());
+        modelAndView.addObject("message", exception.getMessage());
+        return modelAndView;
+    }
 }
--- src/main/java/com/softserve/itacademy/service/impl/UserServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.softserve.itacademy.repository.UserRepository;
 import com.softserve.itacademy.service.UserService;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,25 +21,32 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(User user) {
-            return userRepository.save(user);
+        return userRepository.save(user);
     }
 
     @Override
     public User readById(long id) {
         Optional<User> optional = userRepository.findById(id);
+        if (optional.isPresent()) {
             return optional.get();
+        }
+        throw new EntityNotFoundException("User with id " + id + " not found");
     }
 
     @Override
     public User update(User user) {
-            User oldUser = readById(user.getId());
-                return userRepository.save(user);
+        User oldUser = readById(user.getId());
+        return userRepository.save(user);
     }
 
     @Override
     public void delete(long id) {
         User user = readById(id);
+        if (user != null) {
             userRepository.delete(user);
+        } else {
+            throw new EntityNotFoundException("User with id " + id + " not found");
+        }
     }
 
     @Override
